5aacd55ece6aaf4057cfbcbefe5d09d1abf38627
NagaSakiSoyoLin <1825679368@qq.com>
2026-09-13 23:57:34 +0800

    style(user): 调整 sleep.c 格式与注释

--- user/sleep.c
+++ user/sleep.c
@@ -9,8 +9,9 @@ int main(int argc, char *argv[]) {
     printf("Sleep needs one argument!\n");  // 检查参数数量是否正确
     exit(-1);
   }
+
   int ticks = atoi(argv[1]);  // 将字符串参数转为整数
-  sleep(ticks);               // 使用系统调用sleep
+  sleep(ticks);               // 使用系统调用 sleep
   printf("(nothing happens for a little while)\n");
   exit(0);  // 确保进程退出
 }
